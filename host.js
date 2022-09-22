08c3d1709e8c1e029b2938f2069d011cb1b9f653
João Gabriel Messi Lutz <tibepc@gmail.com>
2022-09-22 12:25:11 -0300

    exclusão do host.js

--- host.js
+++
@@ -1,3 +0,0 @@
-const url = `https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl=${document.URL}type`;
-document.querySelector("#qrcode").src = url;
-document.querySelector("h4").innerHTML = document.URL + 'type';
